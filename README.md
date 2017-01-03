6186b2dbb089c92bec9998369fd5b55c0852ff3c
Sachin <sskamath96@gmail.com>
2017-01-03 12:32:53 +0530

    New Year, New me. Much Wow. Old things rot.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Python Scripts
+
+### gpms.py
+
+Script to apply gate pass from Terminal. 
+
+*To-Do*
+
+* No hard-coding son. Pass values.
+
+### student_vuln.py
+
+A script that uses mechanize to check the number of douchebags in a given range. Again, a script that works in the Intranet.If you are an Amritian, this is for you. Found Douchebags? Tell them. Make the world a secure place to live in.
+
+### soniclogin.py
+
+A script that logs you into the Dell Sonicwall. Customized for Amrita :') 
+
+*To-Do*
+* Passing Arguments
+* Cron
+* Write pip requirements text
+* Current method sucks, use headless browser.
---
+++ internal/gpms.py
@@ -0,0 +1,66 @@
+sebz erdhrfgf vzcbeg frffvba
+vzcbeg heyyvo, heyyvo2, pbbxvryvo,wfba
+tybony ybtva, cbfg1, cbfg2
+# TCZF VF QRNQ. EVC
+
+urnqzrgn = {
+'Npprcg' : 'grkg/ugzy,nccyvpngvba/kugzy+kzy,nccyvpngvba/kzy;d=0.9,vzntr/jroc,*/*;d=0.8',
+'Bevtva' : 'uggc://tczf.nzevgnarg.rqh',
+'K-QriGbbyf-Rzhyngr-Argjbex-Pbaqvgvbaf-Pyvrag-Vq' : '0QP79Q48-6OR3-4P15-N1NP-20O46NN6SN25',
+'Hctenqr-Vafrpher-Erdhrfgf' : '1',
+'Hfre-Ntrag' : 'Zbmvyyn/5.0 (K11; Yvahk k86_64) NccyrJroXvg/537.36 (XUGZY, yvxr Trpxb) Hohagh Puebzvhz/47.0.2526.106 Puebzr/47.0.2526.106 Fnsnev/537.36',
+'Pbagrag-Glcr' : 'nccyvpngvba/k-jjj-sbez-heyrapbqrq',
+'Ersrere' : 'uggc://tczf.nzevgnarg.rqh/tcvf/fghqrag/nccylyrnir.cuc',
+'Npprcg-Rapbqvat' : 'tmvc, qrsyngr',
+'Npprcg-Ynathntr' : 'ra-TO,ra-HF;d=0.8,ra;d=0.6',
+}
+
+ybtva = {
+	'hfrevq' : 'ebyy_ab_urer',
+	'cnffjq' : 'cnffjbeq_urer',
+	'fhozvg' : '',     # Qba'g rqvg
+	}
+
+
+cnlybnqre = {
+'fgertab' : 'PO.RA.H4PFR14240',
+'fganzr' : 'Fnpuva F Xnzngu', 
+'fgubfgry' : 'IO',
+'cnffgnxra' : '',
+'cnffglcr' : 'Qnl Cnff',
+'sebzqngr': '07 Zne 2016',
+'subhe':'7',
+'szva':'00',
+'nccylvatgb':'Jneqra',
+'bppnfvba':'Erthyne Npnqrzvp Frzrfgre',
+'tebhaqfsbeyrnir':'tbvat gb pvgl',
+'yrnirfhozvg':'Fhozvg Yrnir',
+}
+
+cnlybnq2 = {
+'ertab' : 'Ebyy Ahzore urer',     #EBYY
+'fganzr' : 'Anzr urer',
+'nccylvatgb':'Jneqra',
+'cnffglcr' : 'Qnl Cnff',
+'bppnfvba':'Erthyne Npnqrzvp Frzrfgre',
+'tebhaqfsbeyrnir':'tbvat gb pvgl',
+'sebzqngr': '07 Zne 2016 7:00:00', #Fvtu, Qngr    
+'gbqngr': '07 Zne 2016 19:00:00',  #fvtu k2, Qngr
+'abbsqnlf':'1',
+'pbasvez':'Pbasvez',
+}
+
+cevag ybtva()
+jvgu frffvba() nf p:
+    p.urnqref.hcqngr(urnqzrgn)
+    erfcbafr=p.cbfg('uggc://tczf.nzevgnarg.rqh/tcvf/fghqrag/vaqrk.cuc', qngn=ybtva())
+    p.urnqref.hcqngr(urnqzrgn)
+    cevag(erfcbafr.urnqref)
+    #p.cbfg('uggc://tczf.nzevgnarg.rqh/tcvf/fghqrag/nccylyrnir.cuc', qngn=cnlybnqre)
+    #p.urnqref.hcqngr(urnqzrgn)
+    #p.cbfg('uggc://tczf.nzevgnarg.rqh/tcvf/fghqrag/nccylyrnir.cuc', qngn=cnlybnq2)
+    #erfc2=p.trg('uggc://tczf.nzevgnarg.rqh/tcvf/fghqrag/yrnirfgnghf.cuc')
+    #cevag(erfc2.grkg)
+
+
+
